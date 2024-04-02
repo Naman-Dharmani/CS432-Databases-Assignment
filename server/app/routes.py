a3bf8ae542ff956a1fb51af17ee7d90173880a31
Sahil <dassahil@iitgn.ac.in>
2024-04-03 03:17:28 +0530

    added try-except statements, used make_response along with jsonify

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify, request, redirect, url_for
+from flask import render_template, jsonify, request, redirect, url_for, make_response
 from flasgger import swag_from
 
 from . import app
@@ -6,82 +6,116 @@ from .models import User, Product, Listing
 from . import db
 from . import login_manager, login_user, logout_user, login_required, current_user
 
+
+pagination_per_page = 4
+
 # TODO: Home page ideas?
+
+
 @app.route('/')
 def home():
+    """
+    This is the home endpoint.
+    ---
+    responses:
+      200:
+        description: A successful response with "Hello, World!"
+    """
     return "Hello there!"
 
 # <---------------------------------------------Product Routes----------------------------------------------------->
+# TODO: Add try catch error handling in all routes --> DONE
 # TODO: add login_required wherever needed
-
-@app.route('/product/create')
-# @login_required
-def create_product():
-    user_id = current_user.get_id()
-    return render_template('add_product.html')
-
-
-@app.route('/product/create/addToDb', methods=['POST'])
-# @login_required
-def add_product_to_db():
-    user_id = current_user.get_id()
-    data = request.form.to_dict()
-
-    # Adding the form data to the db
-    product = Product(prod_title=data['prod_title'],
-                    description=data['description'],
-                    prod_condition=data['prod_condition'],
-                    listed_price=data['listed_price'],
-                    quantity=data['quantity']
-                    )
-    product.save()
-    return "Product added successfully", 201
+# TODO: use make_response to return response with status code --> DONE
+# TODO: check difference between .get() and .filter_by() in SQLAlchemy --> DONE
+# get() - works only on PK and returns a record
+# filter_by() - works on any column and returns a list of records.
+# Also, multiple conditions can be specified
 
 
-@app.route('/product', methods=['GET'])
+@app.route('/products', methods=['GET'])
 def get_products():
     # Retriving the page number from the query string
-    page = request.args.get('page', 1, type=int)
-    per_page = 4
-    
-    # per_page is the max number of products to be shown
-    # If there are more products, the has_next attribute will be True
-    # IF there are fewer products, the has_next attribute will be False
-    products = Product.query.paginate(page=page, per_page=per_page)
-    products_json = [product.to_dict() for product in products.items]
+    try:
+        page = request.args.get('page', 1, type=int)
 
-    return jsonify({
-        'products': products_json,
-        'has_next': products.has_next  # To Indicate if there are more pages
-    })
+        # per_page is the max number of products to be shown
+        # If there are more products, the has_next attribute will be True
+        # IF there are fewer products, the has_next attribute will be False
+        products = Product.query.paginate(
+            page=page, per_page=pagination_per_page)
+        products_json = [product.to_dict() for product in products.items]
+
+        return make_response(
+            jsonify({
+                    'products': products_json,
+                    'hasnext': products.has_next  # To Indicate if there are more pages
+                    }), 200)
+
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
 
 
-@app.route('/product/<int:id>')
-def product(id, methods=['GET', 'PUT', 'DELETE']):
+@app.route('/product', methods=['POST'])
+# @login_required
+def create_product():
+    try:
+        user_id = current_user.get_id()
+        data = request.json
+        print(request.method)
+        print(data)
+
+        # Adding the form data to the db
+        product = Product(prod_title=data['prod_title'],
+                          description=data['description'],
+                          prod_condition=data['prod_condition'],
+                          listed_price=data['listed_price'],
+                          quantity=data['quantity']
+                          )
+        product.save()
+        return make_response(jsonify({"message": "Product added successfully"}), 201)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
+
+
+@app.route('/product/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def product(id):
     if request.method == 'GET':
-        required_prod = Product.query.filter_by(prod_id=id).first().to_dict()
+        try:
+            required_prod = Product.query.filter_by(
+                prod_id=id).first().to_dict()
+            # If the product is not found, return a 404 error
+            if required_prod == None:
+                return make_response({"message": 'Product not found'}, 404)
+            return make_response(jsonify(required_prod), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 500)
 
-        # If the product is not found, return a 404 error
-        if required_prod == None:
-            return 'Product not found', 404
-        return jsonify(required_prod)
-    elif request.method == 'DELETE':
-        product = Product.query.get(id)
-        if product is None:
-            return jsonify({'message': 'Product not found'}), 404
-        db.session.delete(product)
-        db.session.commit()
-        return jsonify({'message': 'Product deleted'})
     elif request.method == 'PUT':
-        product = Product.query.get(id)
-        if product is None:
-            return jsonify({'message': 'Product not found'}), 404
-        data = request.get_json()
-        for key, value in data.items():
-            setattr(product, key, value)
-        db.session.commit()
-        return jsonify(product.__dict__)
-    
+        try:
+            product = Product.query.get(id)
+            print(product)
+            if product is None:
+                return make_response(jsonify({'message': 'Product not found'}), 404)
+            data = request.get_json()
+            for key, value in data.items():
+                setattr(product, key, value)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Product Updated'}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 500)
+
+    elif request.method == 'DELETE':
+        try:
+            product = Product.query.get(id)
+            if product is None:
+                return make_response(jsonify({'message': 'Product not found'}), 404)
+            db.session.delete(product)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Product deleted'}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 500)
+
 
 # <---------------------------------------------User Routes----------------------------------------------------->
 # TODO
@@ -89,32 +123,47 @@ def product(id, methods=['GET', 'PUT', 'DELETE']):
 @app.route('/user/<int:u_id>', methods=['GET'])
 @swag_from('docs/get_user.yml')
 def get_user(u_id):
-    user = User.query.get(u_id)
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    return jsonify(user.to_dict())
+    try:
+        user = User.query.get(u_id)
+        if user is None:
+            return make_response(jsonify({'message': 'User not found'}), 404)
+        return make_response(jsonify(user.to_dict()), 200)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
+
 
 @app.route('/user/<int:u_id>', methods=['PUT'])
 def update_user(u_id):
-    user = User.query.get(u_id)
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    data = request.get_json()
-    for key, value in data.items():
-        setattr(user, key, value)
-    db.session.commit()
-    return jsonify(user.__dict__)
+    try:
+        user = User.query.get(u_id)
+        if user is None:
+            return make_response(jsonify({'message': 'User not found'}), 404)
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(user, key, value)
+        db.session.commit()
+        return make_response(jsonify(user.to_dict()), 200)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
+
 
 @app.route('/user/<int:u_id>', methods=['DELETE'])
 def delete_user(u_id):
-    user = User.query.get(u_id)
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'User deleted'})
+    try:
+        user = User.query.get(u_id)
+        if user is None:
+            return make_response(jsonify({'message': 'User not found'}), 404)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'User deleted'}))
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
+
 
 @app.route('/user/<int:u_id>/listings', methods=['GET'])
 def get_user_listings(u_id):
-    listings = Listing.query.filter_by(user_id=u_id).all()
-    return jsonify([listing.__dict__ for listing in listings])
+    try:
+        listings = Listing.query.filter_by(user_id=u_id).all()
+        return make_response(jsonify([listing.__dict__ for listing in listings]), 200)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
