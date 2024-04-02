dadce31b2410ff92929a290eb28781a4d5341cf3
Sahil <dassahil@iitgn.ac.in>
2024-04-02 23:33:40 +0530

    dummy datastructure removed and the remaining product routes are implemented

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,83 +1,89 @@
-from flask import render_template, jsonify, request, session, redirect, url_for
+from flask import render_template, jsonify, request, redirect, url_for
 from flasgger import swag_from
 
 from . import app
-from .models import User, Listing
+from .models import User, Product, Listing
 from . import db
-
-# Dummy data structure to simulate a database
-products = [
-     {'prod_id': 1,
-     'prod_title': 'macbook pro',
-     'description': 'A laptop by Apple',
-     'prod_condition': 'new',
-     'listed_price': 100000,
-     'quantity': 1,
-     'date_listed': '2024-03-30',
-     'date_modified': None,
-     'flagging': 0
-     },
-     {'prod_id': 2,
-     'prod_title': 'Nothing Phone 2a',
-     'description': 'Nothing by Nothing',
-     'prod_condition': 'new',
-     'listed_price': 24000,
-     'quantity': 1,
-     'date_listed': '2024-03-30',
-     'date_modified': None,
-     'flagging': 0
-     }
-]
+from . import login_manager, login_user, logout_user, login_required, current_user
 
 # TODO: Home page ideas?
 @app.route('/')
 def home():
     return "Hello there!"
 
-
-# TODO: Add a login route that would store the user_id in the session
-#that session would then be used to know which user has requested/added what product
-
+# <---------------------------------------------Product Routes----------------------------------------------------->
+# TODO: add login_required wherever needed
 
 @app.route('/product/create')
+# @login_required
 def create_product():
+    user_id = current_user.get_id()
     return render_template('add_product.html')
 
 
 @app.route('/product/create/addToDb', methods=['POST'])
+# @login_required
 def add_product_to_db():
+    user_id = current_user.get_id()
+    data = request.form.to_dict()
 
-    # TODO: Once, the login route is implemented, uncomment the code below
-    # if 'user_id' in session:
-    #     user_id = session['user_id']
-        data = request.form.to_dict()
+    # Adding the form data to the db
+    product = Product(prod_title=data['prod_title'],
+                    description=data['description'],
+                    prod_condition=data['prod_condition'],
+                    listed_price=data['listed_price'],
+                    quantity=data['quantity']
+                    )
+    product.save()
+    return "Product added successfully", 201
 
-        # Adding the form data to the db
-        products.append(data)
-        print(products)
 
-        return "Product added successfully", 201
-    # else:
-    #     return redirect(url_for('login'))
+@app.route('/product', methods=['GET'])
+def get_products():
+    # Retriving the page number from the query string
+    page = request.args.get('page', 1, type=int)
+    per_page = 4
+    
+    # per_page is the max number of products to be shown
+    # If there are more products, the has_next attribute will be True
+    # IF there are fewer products, the has_next attribute will be False
+    products = Product.query.paginate(page=page, per_page=per_page)
+    products_json = [product.to_dict() for product in products.items]
+
+    return jsonify({
+        'products': products_json,
+        'has_next': products.has_next  # To Indicate if there are more pages
+    })
 
 
 @app.route('/product/<int:id>')
-def product(id, methods=['GET']):
+def product(id, methods=['GET', 'PUT', 'DELETE']):
+    if request.method == 'GET':
+        required_prod = Product.query.filter_by(prod_id=id).first().to_dict()
 
-    # TODO: Replace this with a database query
-    required_prod = {}
-    for prod in products:
-        if prod['prod_id'] == id:
-            required_prod = prod
-            break
+        # If the product is not found, return a 404 error
+        if required_prod == None:
+            return 'Product not found', 404
+        return jsonify(required_prod)
+    elif request.method == 'DELETE':
+        product = Product.query.get(id)
+        if product is None:
+            return jsonify({'message': 'Product not found'}), 404
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': 'Product deleted'})
+    elif request.method == 'PUT':
+        product = Product.query.get(id)
+        if product is None:
+            return jsonify({'message': 'Product not found'}), 404
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(product, key, value)
+        db.session.commit()
+        return jsonify(product.__dict__)
+    
 
-    # If the product is not found, return a 404 error
-    if not required_prod:
-        return 'Product not found', 404
-    return jsonify(required_prod)
-
-
-# USER ROUTES
+# <---------------------------------------------User Routes----------------------------------------------------->
 # TODO
 
 @app.route('/user/<int:u_id>', methods=['GET'])
